84aab405e71ed0c197803a00b57674251fad28a5
zack <zack>
2022-01-01 19:14:12 +0530

    ColorChanges in config

---
+++ src/components/ColorDot.js
@@ -0,0 +1,7 @@
+const ColorDot = ({color}) => {
+    return (
+        <div style={{'border-radius':'100%','background-color':color, 'width':'1em','height':'1em', 'margin':'0.3em'}}></div>
+    );
+};
+
+export default ColorDot;
--- src/components/GameConfigurator.js
+++ src/components/GameConfigurator.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import ColorDot from './ColorDot.js';
 const GameConfigurator = () =>{
-    const [numColors, setNumColors] = React.useState(6);
-    const decColors = () => {
-        setNumColors(numColors => numColors==1?numColors:numColors-1);
+    const [colors, setColors] = React.useState([
+        "#1f9e30", // green
+        "#e50d23", // red
+        "#e8e8e8", // white
+        "#101010", // black
+        "#e1e82e", // yellow
+        "#e82ecc", // pink
+    ]);
+    const [newColor, setNewColor] = React.useState("#000000");
+    const [validColor, setValidColor] = React.useState(true);
+    const updateColorBox = (event) => {
+        setNewColor(event.target.value);
+        let pattern = /^[#][abcdef0123456789]{6,6}$/;
+        setValidColor(pattern.match(event.target.value));
     };
-    const incColors = () => {
-        setNumColors(numColors => numColors+1);
-    };
+
     return (
     <div>
         <p>GC</p>
-        
-        <p>{numColors}</p>
+        <p>{colors.length}</p>
+        {colors.map(col => (
+            <div>
+                <ColorDot color={col}/>
+                <button>-</button>
+            </div>
+        ))}
+        <textarea value={newColor} onChange={updateColorBox}/>
+        <button style={{'backgroundColor':validColor?null:"#e00000"}}>Add new color</button>
     </div>
     );
 };
