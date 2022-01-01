b62f6a55afd9431218608295f240d9e80e0a544d
zack <zack>
2022-01-01 18:41:04 +0530

    Printing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import GamePlayer from './components/GamePlayer.js';
 import GameConfigurator from './components/GameConfigurator.js';
+import MasterAlgo from './MasterAlgo.js';
 function App() {
+    console.log(MasterAlgo.getAllStartingCodes(4,false));
     return (
     <div className="App">
        <p>App</p>
+        
         <GameConfigurator/>
       </div>
   );
