bccef7427c7984678326d1e51b16bebc11181fd1
Henry Porter <henryporter@Henrys-MacBook-Pro.local>
2022-01-01 18:35:44 +0530

    get all starting codes

--- src/MasterAlgo.js
+++ src/MasterAlgo.js
@@ -7,7 +7,7 @@ let MasterAlgo = {
         if (num==0) return 'r';
         if (num==1) return 'y';
         if (num==2) return 'g';
-        if (num==3) return 'b';
+        if (num==3) return 'bl';
         if (num==4) return 'b';
         if (num==5) return 'w';
         return 'error in num';
@@ -15,19 +15,31 @@ let MasterAlgo = {
     //TODO
     convertStrToNum: (str) => {
         if (str=='r') return 0;
-        if (str=='r') return 0;
-        if (str=='r') return 0;
-        if (str=='r') return 0;
-        if (str=='r') return 0;
-        if (str=='r') return 0;
-        if (str=='r') return 0;
+        if (str=='y') return 0;
+        if (str=='g') return 0;
+        if (str=='bl') return 0;
+        if (str=='b') return 0;
+        if (str=='w') return 0;
         return 'error in str';
     },
     //TODO
     getAllStartingCodes: (numberOfColors, allowRepeatedColors)=>{
         // returns an array containing all of the possible mastermind codes in existance.
         // ex: [ [0,1,2,3], [0,1,2,4],.... [9,9,9,9] ]
-    },
+        let possibleCodes= [];
+        if (allowRepeatedColors==false){
+            for (let x=0; x <(numberOfColors); x++){
+                for (let y = 0; y<(numberOfColors); y++){
+                    for (let z = 0; z<(numberOfColors); z++){
+                        for (let w = 0; w<(numberOfColors); w++){
+                            possibleCodes.push([x,y,z,w]);
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 };
 
